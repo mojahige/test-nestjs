22f5a01dd50eb67893afee5454c615efc2b85672
mojahige <mojahige@gmail.com>
2020-11-27 00:10:38 +0900

    Add methods to the user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Param,
+  Get,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { Prisma, User } from '@prisma/client';
 
@@ -7,12 +15,30 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async users(): Promise<User[]> {
+  async findAll(): Promise<User[]> {
     return this.userService.users();
   }
 
   @Post()
   async create(@Body() params: Prisma.UserCreateInput): Promise<User> {
     return this.userService.createUser(params);
+  }
+
+  @Get(':id')
+  async findUser(@Param('id') id: string): Promise<User | null> {
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body('name') name: string): Promise<User> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: { name },
+    });
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<User> {
+    return this.userService.deleteUser({ id: Number(id) });
   }
 }
